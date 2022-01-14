d4f595d5ab818725ddd571d57adba90926ed9cd3
Will W <wjwat@onionslice.org>
2022-01-14 12:05:10 -0600

    Add logic to determine favored language

--- js/main.js
+++ js/main.js
@@ -1,11 +1,36 @@
-function myLog(s) {
-    console.log(s);
+let myLog = console.log;
+
+function getVal(sel) {
+    return $(sel).val();
 }
 
 $(document).ready(function() {
 	$('form').submit(function(e) {
 		e.preventDefault();
+        let username = getVal('input#username');
+        let bdfl = getVal('input:radio[name="bdfl"]:checked');
+        let typing = getVal('input:radio[name="typing"]:checked');
+        let color = getVal('select#colors option:selected');
+        let indentation = getVal('select#indentation option:selected');
+        let meal = getVal('select#meal option:selected');
 
-        myLog('We submit!');
+        // python, ruby, javascript
+        let py = 0;
+        let ru = 0;
+        let js = 0;
+
+        // let color = $('select#colors option:selected');
+
+        [bdfl, typing, color, indentation, meal].forEach(function(item) {
+            if (color === 'Python') {
+                py += 1;
+            } else if (color === 'Ruby') {
+                ru += 1;
+            } else {
+                js += 1;
+            }
+        });
+
+        myLog(py, ru, js);
 	});
 });
