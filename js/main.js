9175e8df5061373f3178caa2bb5f9e89dc0f79d1
Will W <wjwat@onionslice.org>
2022-01-14 13:00:10 -0600

    prefer certain languages for user by default

--- js/main.js
+++ js/main.js
@@ -4,10 +4,15 @@ function getVal(sel) {
     return $(sel).val();
 }
 
+// In cases which a language is equal to another we
+// strongly prefer them in order passed.
+//
+// ex: py = 2, and ru = 2, then prefer Python
+//     js = 2, and ru = 2, then prefer Ruby
 function determinePreferredLanguage(py, ru, js) {
-    if (py > js && py > ru) {
+    if (py >= js && py >= ru) {
         return "Python";
-    } else if (ru > js && ru > py) {
+    } else if (ru >= js && ru >= py) {
         return "Ruby";
     } else {
         return "JavaScript";
@@ -18,6 +23,7 @@ $(document).ready(function() {
 	$('form').submit(function(e) {
 		e.preventDefault();
         let username = getVal('input#username');
+
         let bdfl = getVal('input:radio[name="bdfl"]:checked');
         let typing = getVal('input:radio[name="typing"]:checked');
         let color = getVal('select#colors option:selected');
@@ -27,11 +33,11 @@ $(document).ready(function() {
         myLog(bdfl, typing, color, indentation, meal);
 
         // python, ruby, javascript
-        let py = 1;
+        let py = 0;
         let ru = 0;
         let js = 0;
         myLog(py, ru, js);
-        // 
+
         [bdfl, typing, color, indentation, meal].forEach(function(item) {
             if (item === 'Python') {
                 py += 1;
@@ -44,6 +50,6 @@ $(document).ready(function() {
 
         let prefLanguage = determinePreferredLanguage(py, ru, js);
 
-        $('div#output h2').html('<center>Your preferred language is: '+prefLanguage+'!</center>')
+        $('div#output h2').html('<center>Thank you, '+ username +'. Your preferred language is: '+prefLanguage+'!</center>')
 	});
 });
